fae63ba97248acc98a4e81614aaf4b0d6da49d5a
SUCHMOKUO <suchmokuo@gmail.com>
2020-09-26 00:24:32 +0800

    [liuxing] 4.3 前端：完成页面的路由功能

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,33 +1,46 @@
 import React from "react";
-import "./App.css";
-import { Header, HeaderItem, Footer } from "./components";
-import { AddProduct, Mall, Order } from "./pages";
 import { HiOutlineHome } from "react-icons/hi";
 import { AiOutlineShoppingCart } from "react-icons/ai";
 import { BsPlus } from "react-icons/bs";
+import { Header, HeaderItem, Footer } from "./components";
+import { AddProduct, Mall, Order } from "./pages";
+import "./App.scss";
+import {
+  Redirect,
+  Route,
+  Switch,
+  BrowserRouter as Router,
+} from "react-router-dom";
 
 function App() {
   return (
-    <>
+    <Router>
       <Header>
-        <HeaderItem>
+        <HeaderItem to="/mall">
           <HiOutlineHome />
           商城
         </HeaderItem>
-        <HeaderItem>
+        <HeaderItem to="/orders">
           <AiOutlineShoppingCart />
           订单
         </HeaderItem>
-        <HeaderItem>
+        <HeaderItem to="/add-product">
           <BsPlus />
           添加商品
         </HeaderItem>
       </Header>
 
-      <main></main>
+      <main>
+        <Switch>
+          <Route exact path="/mall" component={Mall} />
+          <Route exact path="/orders" component={Order} />
+          <Route exact path="/add-product" component={AddProduct} />
+          <Redirect from="/*" to="/mall" />
+        </Switch>
+      </main>
 
       <Footer text="TW Mall &copy;2018 Created by ForCheng" />
-    </>
+    </Router>
   );
 }
 
--- front-end/src/components/Header/HeaderItem.js
+++ front-end/src/components/Header/HeaderItem.js
@@ -1,14 +1,35 @@
 import React, { Component } from "react";
+import { withRouter } from "react-router";
 import "./HeaderItem.scss";
 
-export class HeaderItem extends Component {
-  render() {
-    const { children, onClick } = this.props;
+export const HeaderItem = withRouter(
+  class extends Component {
+    get active() {
+      const {
+        to,
+        location: { pathname },
+      } = this.props;
 
-    return (
-      <div className="header-item" onClick={onClick}>
-        {children}
-      </div>
-    );
+      return to === pathname;
+    }
+
+    onItemClick = () => {
+      const { history, to } = this.props;
+
+      history.push(to);
+    };
+
+    render() {
+      const { children } = this.props;
+      const { onItemClick, active } = this;
+
+      const className = active ? "header-item active" : "header-item";
+
+      return (
+        <div className={className} onClick={onItemClick}>
+          {children}
+        </div>
+      );
+    }
   }
-}
+);
